f57bce740241b37291bae20562ae7d882df52f9b
Arturo Lopez <aarturock92@gmail.com>
2017-06-02 18:20:46 -0500

    Implement CRUD Movil Service

--- app/shared/services/movil.service.ts
+++ app/shared/services/movil.service.ts
@@ -13,13 +13,72 @@ import { AuthenticationService } from '../utils/authentication.service'
 import { DataService } from './data.service'
 
 @Injectable()
-export class MovilService{
+export class MovilService extends DataService{
     private _baseUrl: string
     private _uriMovil: string ="Movil"
 
     constructor(private http: Http,
-                private configService: ConfigService){}
+                private configService: ConfigService,
+                private authenticationService: AuthenticationService){
 
-    getMovil
+        super()
 
+        this._baseUrl = configService.getApiURI()
+    }
+
+    getMovilesByEstatus(estatusRegistro:number){
+        return this.http.get(this._baseUrl + this._uriMovil + '/list?estatusRegistro='+estatusRegistro, {headers: this.authenticationService.getHeaders()})
+               .map((res: Response) => {
+                   return res.json()
+               })
+               .catch(this.handleError)
+    }
+
+    getMovilesPagination(page?:number, itemsPerPage?:number):Observable<PaginatedResult<IMovil[]>>{
+        let paginatedResult: PaginatedResult<IMovil[]> = new PaginatedResult<IMovil[]>()
+
+        return this.http.get(this._baseUrl + this._uriMovil + '/search/'+page+'/'+itemsPerPage, { headers: this.authenticationService.getHeaders()})
+                .map((res: Response) => {
+                    let data = res.json();
+                    paginatedResult.count = data.count
+                    paginatedResult.page = data.page
+                    paginatedResult.result = data.items
+                    paginatedResult.totalCount = data.totalCount
+                    paginatedResult.totalPages = data.totalPages
+                    return paginatedResult
+                })
+                .catch(this.handleError)
+    }
+
+    getMovilDetails(id:number): Observable<IMovil> {
+        return this.http.get(this._baseUrl + this._uriMovil + '/'+id, {headers: this.authenticationService.getHeaders()} )
+               .map((res: Response) => {
+                    return res.json()
+               })
+               .catch(this.handleError)
+    }
+
+    createMovil(movil: IMovil): Observable<IMovil>{
+        return this.http.post(this._baseUrl + this._uriMovil, JSON.stringify(movil), {headers: this.authenticationService.getHeaders()} )
+               .map((res: Response) => {
+                   return res.json()
+               })
+               .catch(this.handleError)
+    }
+
+    deleteMovil(id:number): Observable<void>{
+        return this.http.delete(this._baseUrl + this._uriMovil + '/'+id, {headers: this.authenticationService.getHeaders() })
+               .map((res: Response) => {
+                   return;
+               })
+               .catch(this.handleError);
+    }
+
+    updateMovil(id:Number, movil: IMovil): Observable<IMovil>{
+        return this.http.put(this._baseUrl + this._uriMovil, JSON.stringify(movil), {headers: this.authenticationService.getHeaders()})
+               .map((res: Response) => {
+                    return res.json()
+               })
+               .catch(this.handleError)
+    }
 }
